a4c2dd8f543abc0ee0d5abd63d7cb1e10425181f
Sir-josh <docsmile01@gmail.com>
2025-03-03 10:39:20 +0100

    modified the read.Me and added screenshots

--- script.js
+++ script.js
@@ -1,30 +1,38 @@
-const submitBtn = document.querySelector('.btn')
-let value = document.querySelector('.value')
-const output = document.querySelector('.output');
+const submitBtn = document.querySelector(".btn");
+let value = document.querySelector(".value");
+const output = document.querySelector(".output");
+const wrapper = document.querySelector('.wrapper');
+const secWrapper = document.querySelector('.sec-wrapper');
+const buttons = document.querySelectorAll(".num");
 
-const buttons = document.querySelectorAll('.num');
-  buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-      buttons.forEach(b => {
-        b.classList.add('num-active');
-        button.classList.remove('num-active');
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    buttons.forEach((b) => {
+      b.classList.add("num-active");
+      button.classList.remove("num-active");
 
-        value = button.id;
-      })
-    })
-  })
+      value = button.id;
+    });
+  });
+});
+
+submitBtn.addEventListener("click", () => {
+  if (!value) {
+    alert('Please pick a number for rating');
+    return;
+  } 
+
+  wrapper.classList.add('wrapper-active');
+  secWrapper.classList.add('sec-wrapper-active');
 
 
-submitBtn.addEventListener('click', () => {
-  const secondWrap = document.querySelector('.second-page');
-  // secondWrap.classList.add('second-page-active');
   if (value === null) {
     alert('Please select a value');
 
   } else {
     output.innerHTML = `You selected ${value} out of 5`
-  secondWrap.classList.add('second-page-active');
-
   }
 
-})
+});
+
+
